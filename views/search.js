d84dbf55f1f0f9266c964cc37a82cbb818656ba9
PRadermecker <pradermecker@cirb.irisnet.be>
2010-02-25 11:29:33 +0100

    SplitView for SearchView

--- views/search.js
+++ views/search.js
@@ -10,32 +10,34 @@
 
   @extends SC.ScrollView
 */
-Nestor.SearchView = SC.ScrollView.design(
-	/** @scope Nestor.SearchView.prototype */ {
+Nestor.SearchView = SC.SplitView.design({
+	/** @scope Nestor.SearchView.prototype */ 
 
-		 // TODO: Add your own code here.
-	 hasHorizontalScroller: NO,
-	 layout: { top: 36, bottom: 32, left: 0, right: 0 },
-	 backgroundColor: 'white',
-	 contentView: SC.TableView.design({
-		 columns: [
-			 SC.TableColumn.create({
-				 key:   'name',
-				 label: 'Nom',
-				 width: 500
-			 }),
-			 SC.TableColumn.create({
-				 key:   'managerName',
-				 label: 'Gestionnaire',
-				 width: 200,
-				 minWidth: 150
-			 })
-			 ],
-			 // flexibleColumn:   tableColumns.objectAt(0),
-			 contentBinding:   'Nestor.filesController.arrangedObjects',
-			 selectionBinding: 'Nestor.filesController.selection',
-			 canReorderContent: YES,
-			 exampleView: SC.TableRowView,
-			 recordType: Nestor.File
-	 })
+	 // TODO: Add your own code here.
+	hasHorizontalScroller: NO,
+	layout: { top: 36, bottom: 32, left: 0, right: 0 },
+	backgroundColor: 'white',
+	defaultThickness: 0.2,
+	layoutDirection: SC.LAYOUT_VERTICAL,
+	bottomRightView: SC.TableView.design({
+		columns: [
+			SC.TableColumn.create({
+				key:   'name',
+				label: 'Nom',
+				width: 500
+			}),
+			SC.TableColumn.create({
+				key:   'managerName',
+				label: 'Gestionnaire',
+				width: 200,
+				minWidth: 150
+			})
+			],
+			// flexibleColumn:   tableColumns.objectAt(0),
+			contentBinding:   'Nestor.filesController.arrangedObjects',
+			selectionBinding: 'Nestor.filesController.selection',
+			canReorderContent: YES,
+			exampleView: SC.TableRowView,
+			recordType: Nestor.File	
+		})
 });
